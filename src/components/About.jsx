3080ac2d991691479f1b87223e57d9626e773664
ArsalanShaiikh <139189175+ArsalanShaiikh@users.noreply.github.com>
2025-09-03 13:36:08 +0530

    Mohit to Mojito spelling correction

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,12 @@ import {SplitText} from "gsap/all";
 
 const About = () => {
 
-    useGSAP(()=>{
-        const titleSplit= SplitText.create('#about h2', {
-            type:'words'
+    useGSAP(() => {
+        const titleSplit = SplitText.create('#about h2', {
+            type: 'words'
         })
 
-        const scrollTimeline= gsap.timeline({
+        const scrollTimeline = gsap.timeline({
             scrollTrigger: {
                 trigger: "#about",
                 start: 'top center'
@@ -20,8 +20,8 @@ const About = () => {
         scrollTimeline
 
             .from(titleSplit.words, {
-            opacity:0, duration:1, yPercent: 100, ease: 'expo.out', stagger: 0.02
-        })
+                opacity: 0, duration: 1, yPercent: 100, ease: 'expo.out', stagger: 0.02
+            })
             .from('.top-grid div, .bottom-grid div', {
                 opacity: 0, duration: 1, ease: 'power1.inOut', stagger: 0.04,
             }, '-=0.5')
@@ -48,10 +48,11 @@ const About = () => {
                         <div>
                             <p className="md:text-3xl text-xl font-bold">
                                 <span>4.5</span>/5
-                                <p className="text-sm text-white-100">
-                                    More than +12000 customers
-                                </p>
                             </p>
+                            <p className="text-sm text-white-100">
+                                More than +12000 customers
+                            </p>
+
                         </div>
                     </div>
                 </div>
